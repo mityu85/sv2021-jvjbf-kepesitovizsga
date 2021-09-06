b7f043fe40fc47c12e0c7fa77e6b9fb5d52bb66f
Mihaly Toth <Mihaly.Toth@kuka.com>
2021-09-06 12:03:46 +0200

    first commit

---
+++ src/main/java/training360/Sv2021JvjbfKepesitovizsgaApplication.java
@@ -0,0 +1,13 @@
+package training360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv2021JvjbfKepesitovizsgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sv2021JvjbfKepesitovizsgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/training360/Sv2021JvjbfKepesitovizsgaApplicationTests.java
@@ -0,0 +1,13 @@
+package training360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sv2021JvjbfKepesitovizsgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
